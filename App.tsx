b7ac8d7f05d6d9027aedb95ee03ec525c6549e84
Andres <andres.porfirio.tablet@gmail.com>
2023-08-15 14:34:29 -0300

    base example

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>
+        Open up App.tsx to start working on your app!
+      </Text>
+
+      <TouchableOpacity style={styles.button}>
+        <Text>Click-Me</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  button: {
+    backgroundColor: "yellow",
   },
 });

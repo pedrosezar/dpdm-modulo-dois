14b90d17c35b037e3ee23429bc7f718087a9cc4b
Andres <andres.porfirio.tablet@gmail.com>
2023-08-15 15:13:23 -0300

    replaced default styles by tailwind classes

--- app/Home.tsx
+++ app/Home.tsx
@@ -4,28 +4,17 @@ import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import "../global.css";
 
 export default function Home() {
-  return <View className="flex-1 items-center justify-center bg-red-500" />;
-  //   return (
-  //     <View style={styles.container}>
-  //       <Text style={styles.title}>
-  //         Open up App.tsx to start working on your app!
-  //       </Text>
+  return (
+    <View className="flex-1 items-center justify-center bg-red-200 ">
+      <Text className="font-bold text-xl text-center ">
+        Open up App.tsx to start working on your app!
+      </Text>
 
-  //       <TouchableOpacity style={styles.button}>
-  //         <Text>Click-Me</Text>
-  //       </TouchableOpacity>
+      <TouchableOpacity className="bg-yellow-700 p-4 rounded-lg mt-4 ">
+        <Text className="text-white font-bold ">Click-Me</Text>
+      </TouchableOpacity>
 
-  //       <StatusBar style="auto" />
-  //     </View>
-  //   );
+      <StatusBar style="auto" />
+    </View>
+  );
 }
-
-const styles = StyleSheet.create({
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  button: {
-    backgroundColor: "yellow",
-  },
-});
